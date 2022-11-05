92d83c6fbe110f304a94427eda443f46ffd08acc
didierguy <did17486@hotmail.fr>
2022-11-05 18:14:57 +0100

    ajout users.js

---
+++ app.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+const port = process.env.APP_PORT ?? 5000;
+
+const welcome = (req, res) => {
+  res.send("Welcome to my favourite movie list");
+};
+
+app.get("/", welcome);
+
+const movieHandlers = require("./movieHandlers");
+const users = require("./users")
+
+app.get("/api/movies", movieHandlers.getMovies);
+app.get("/api/movies/:id", movieHandlers.getMovieById);
+
+app.get("/api/users", users.getUsers);
+app.get("/api/users/:id", users.getUserById);
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    console.log(`Server is listening on ${port}`);
+  }
+});
